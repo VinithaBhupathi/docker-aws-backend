c2651e53da514db7d39d72a04e3b35258d5d8274
grkraoganga <grkraoganga@gmail.com>
2025-09-15 19:42:55 +0530

    first commit

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://ec2-3-108-194-121.ap-south-1.compute.amazonaws.com:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
